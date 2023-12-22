67a54c607c21a82443920d5f5b3729a07faad9ab
GoranStetin <stetinns@gmail.com>
2023-12-22 13:03:55 +0100

    Step2. Current conditions tabs and components logic

--- src/app/main-page/current-conditions/current-conditions/current-conditions.component.ts
+++ src/app/main-page/current-conditions/current-conditions/current-conditions.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, inject} from '@angular/core';
+import {Component, Input, OnInit, inject} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -6,12 +6,19 @@ import {Router} from "@angular/router";
   templateUrl: './current-conditions.component.html',
   styleUrls: ['./current-conditions.component.css']
 })
-export class CurrentConditionsComponent {
+export class CurrentConditionsComponent implements OnInit {
 
   /* Input specific location data with current condition */
   @Input() location;
 
+  /* Input weather icon */
+  @Input() weatherIcon;
+
   private router = inject(Router); 
+
+  ngOnInit(): void {
+    console.log(this.location)
+  }
 
   showForecast(zipcode : string){
     this.router.navigate(['/forecast', zipcode])
