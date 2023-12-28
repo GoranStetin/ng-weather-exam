1ff30ab0bf30ab2b1a63b76f94351f4eb7853261
GoranStetin <stetinns@gmail.com>
2023-12-28 13:41:40 +0100

    Making tabs tamlate reusable

--- src/app/main-page/current-conditions/current-conditions/current-conditions.component.ts
+++ src/app/main-page/current-conditions/current-conditions/current-conditions.component.ts
@@ -1,20 +1,16 @@
-import {Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-current-conditions',
   templateUrl: './current-conditions.component.html',
   styleUrls: ['./current-conditions.component.css']
 })
-export class CurrentConditionsComponent implements OnInit {
+export class CurrentConditionsComponent {
 
   /* Input specific location data with current condition */
   @Input() location;
 
   /* Input weather icon */
   @Input() weatherIcon;
-
-  ngOnInit(): void {
-    console.log(this.location)
-  }
 
 }
--- src/app/shared/tab-container/tab-container.component.ts
+++ src/app/shared/tab-container/tab-container.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, AfterViewInit, ChangeDetectorRef, Component, ContentChildren, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, Output, QueryList, SimpleChanges, TemplateRef, ViewChildren } from '@angular/core';
 import { TabTitle } from '../models/sharedTypes';
 import { TabComponent } from './tab/tab.component';
 
@@ -13,32 +13,21 @@ export class TabContainerComponent implements AfterViewInit {
   // cdr will manually detect changes after updating a child component's property
   constructor(private cdr: ChangeDetectorRef) { }
 
-
   @ViewChildren(TabComponent) tabs: QueryList<TabComponent>;
 
   // input titles that will be shown in the tabs
   @Input() titles: TabTitle[];
 
+  // input contentTemplates
+  @Input() contentTemplates: TemplateRef<[]>[];
+
+  // input tabContexts
+  @Input() tabContexts: [];
+
   // Output event emitter for tab close. Emits the data associated with the tab.
-  @Output() onTabClosed: EventEmitter<TabTitle> = new EventEmitter();
+  @Output() onTabClosed: EventEmitter<number> = new EventEmitter();
 
-  // Output event emitter for tab selection. Emits the data associated with the tab.
-  @Output() onTabSelected: EventEmitter<TabTitle> = new EventEmitter();
-
-  // Method to handle close icon click events on the tab. Emits the tab's data for closure.
-  onCloseClicked(event: TabTitle) {
-    this.onTabClosed.emit(event);
-  }
-
-  // Method to handle click on tab. Emits the tab's data for selection.
-  selectTab(event: TabTitle) {
-    this.onTabSelected.emit(event);
-    const selectedTab = this.tabs.find(tab => tab.title.title === event.title);
-    if (selectedTab) {
-      this.setTabActive(selectedTab);
-    }
-
-  }
+  selectedTabIndex = 0;
 
   ngAfterViewInit() {
     // Check if any tab is has activ true, if not it will set the first one as active;
@@ -46,6 +35,21 @@ export class TabContainerComponent implements AfterViewInit {
     if (activeTabs.length === 0) {
       this.setTabActive(this.tabs.first);
     }
+  }
+
+  // Method to handle close icon click events on the tab. Emits the tab's data for closure.
+  onCloseClicked(event: TabTitle, index: number) {
+    this.onTabClosed.emit(index);
+    this.selectTab(event, 0)
+  }
+
+  // Method to handle click on tab. Emits the tab's data for selection.
+  selectTab(event: TabTitle, index: number) {
+    const selectedTab = this.tabs.find(tab => tab.title.title === event.title);
+    if (selectedTab) {
+      this.setTabActive(selectedTab);
+    }
+    this.selectedTabIndex = index;
   }
 
   //  Method to set tab active
